bfe565ac8bf028a3828c15bc2383808ff03fe9b2
Jan Szczepan <janszczepan62@gmail.com>
2022-03-23 07:51:21 +0100

    Smart contracts basic structure created with hardhat

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEthereum } from '@fortawesome/free-brands-svg-icons'
 import { faInfo } from '@fortawesome/free-solid-svg-icons'
-import bootstrap from 'bootstrap'
+import bootstrap from 'bootstrap/dist/js/bootstrap'
 
 import './Transactions.scss'
 

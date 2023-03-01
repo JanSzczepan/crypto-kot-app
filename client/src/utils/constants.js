c177e6457b74c5d87591a6335862fdf9f60bf97c
Jan Szczepan <janszczepan62@gmail.com>
2023-03-01 22:59:02 +0100

    Migration to goerli testnet

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-import abi from './Transactions.json';
+import abi from "./Transactions.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x3fBFD5A680C2C115A6815CAA781e906238ec97Bd';
+export const contractAddress = "0x22fE31c4E94d90f46BC94b1a1D0b6d37b0044Ce4";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,15 @@
 // https://eth-ropsten.alchemyapi.io/v2/KCaaXBfzjjCniIaEfWHEqvFErBZN_7ie
 
-require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: '0.8.0',
-  networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/KCaaXBfzjjCniIaEfWHEqvFErBZN_7ie',
-      accounts: ['00b5e0097e0b76420c50b244050437924534ec666d4fc47a901a0cdad3b4bee2']
-    }
-  }
-}
+	solidity: "0.8.0",
+	networks: {
+		goerli: {
+			url: "https://eth-goerli.g.alchemy.com/v2/JW2ISmKC-hY7LuIEepAJLfvXKOVmEPdw",
+			accounts: [
+				"286127c966e4774205c971ece55a0baab741df068f95a72431ecf8a167e0854b",
+			],
+		},
+	},
+};
